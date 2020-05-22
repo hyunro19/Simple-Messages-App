0ed723e9bff1ef42aef8eb2a30947191a8b18c75
hyunro19 <hyunro19@gmail.com>
2020-05-22 18:10:13 +0900

    200522 | added SpringMVC, Filter

--- src/main/java/app/messages/Application.java
+++ src/main/java/app/messages/Application.java
@@ -1,12 +1,21 @@
 package app.messages;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args)     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        MessageService messageService = context.getBean(MessageService.class);
-        messageService.save("Hello, Spring!");
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        // SpringBoot의 컨테이너 구동 코드
+        // 이제 MessageService.java는 필요 없다.
+        
+
+        // SpringBoot가 아닌 SpringContainer 구동 코드
+        // ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        // MessageService messageService = context.getBean(MessageService.class);
+        // messageService.save("Hello, Spring!");
     }
 }
--- src/main/java/app/messages/MessageService.java
+++ src/main/java/app/messages/MessageService.java
@@ -1,11 +1,24 @@
 package app.messages;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageService {
+    // @Autowired 필드 기반 주입은 사용하지 말자
     private MessageRepository repository;
 
+    @Autowired
     public MessageService (MessageRepository repository) {
         this.repository = repository;
     }
+
+    // @Required
+    // public void setRepository(MessageRepository repository) {
+    //     this.repository = repository;
+    // }
+
     public void save(String text) {
         this.repository.saveMessage(new Message(text));
     }
