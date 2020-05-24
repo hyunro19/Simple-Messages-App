04ee53c9d9c6c395c4db4af25665d7e4f8925a70
hyunro19 <hyunro19@gmail.com>
2020-05-24 12:58:22 +0900

    200524 | tested Hibernate

--- src/main/java/app/messages/AppConfig.java
+++ src/main/java/app/messages/AppConfig.java
@@ -1,16 +1,24 @@
 package app.messages;
 
 import java.util.Arrays;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
 @ComponentScan("app.messages")
 public class AppConfig {
 
+  private DataSource dataSource;
+
+  public AppConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
   @Bean
   public FilterRegistrationBean<AuditingFilter> auditingFilterRegistrationBean() {
     FilterRegistrationBean<AuditingFilter> registration = new FilterRegistrationBean<>();
@@ -19,5 +27,13 @@ public class AppConfig {
     registration.setOrder(Integer.MAX_VALUE);
     registration.setUrlPatterns(Arrays.asList("/messages/*"));
     return registration;
+  }
+
+  @Bean
+  LocalSessionFactoryBean sessionFactory() {
+    LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+    sessionFactoryBean.setDataSource(dataSource);
+    sessionFactoryBean.setPackagesToScan("app.messages");
+    return sessionFactoryBean;
   }
 }
--- src/main/java/app/messages/MessageRepository.java
+++ src/main/java/app/messages/MessageRepository.java
@@ -1,46 +1,49 @@
 package app.messages;
 
-import javax.sql.DataSource;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRepository {
-  private static final Log logger = LogFactory.getLog(MessageRepository.class);
-  private NamedParameterJdbcTemplate jdbcTemplate;
-  @Autowired
-  public void setDataSource(DataSource dataSource) {
-    this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+  private SessionFactory sessionFactory;
+  public MessageRepository(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
   }
-
-  private DataSource dataSource;
-
-  public MessageRepository(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
   public Message saveMessage(Message message) {
-    GeneratedKeyHolder holder = new GeneratedKeyHolder();
-    
-    // 생성된 레코드의 키값을 받을 객체 및 SQL에 들어갈 파라미터들
-    MapSqlParameterSource params = new MapSqlParameterSource();
-    params.addValue("text", message.getText());
-    params.addValue("createdDate", message.getCreatedDate());
-    String insertSQL = "INSERT INTO messages (`id`, `text`, `created_date`) VALUE (null, :text, :createdDate)";
-    try {
-      this.jdbcTemplate.update(insertSQL, params, holder);
-    } catch (DataAccessException e) {
-      logger.error("Failed to save message", e);
-      return null;
-    }
-    // 생성된 레코드의 내용을 담은 해당하는 객체
-    return new Message(holder.getKey().intValue(), message.getText(), message.getCreatedDate());
+    Session session = sessionFactory.openSession();
+    session.save(message);
+    return message;
   }
+
+  // private static final Log logger = LogFactory.getLog(MessageRepository.class);
+  // private NamedParameterJdbcTemplate jdbcTemplate;
+  // @Autowired
+  // public void setDataSource(DataSource dataSource) {
+  //   this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+  // }
+
+  // private DataSource dataSource;
+
+  // public MessageRepository(DataSource dataSource) {
+  //   this.dataSource = dataSource;
+  // }
+
+  // public Message saveMessage(Message message) {
+  //   GeneratedKeyHolder holder = new GeneratedKeyHolder();
+    
+  //   // 생성된 레코드의 키값을 받을 객체 및 SQL에 들어갈 파라미터들
+  //   MapSqlParameterSource params = new MapSqlParameterSource();
+  //   params.addValue("text", message.getText());
+  //   params.addValue("createdDate", message.getCreatedDate());
+  //   String insertSQL = "INSERT INTO messages (`id`, `text`, `created_date`) VALUE (null, :text, :createdDate)";
+  //   try {
+  //     this.jdbcTemplate.update(insertSQL, params, holder);
+  //   } catch (DataAccessException e) {
+  //     logger.error("Failed to save message", e);
+  //     return null;
+  //   }
+  //   // 생성된 레코드의 내용을 담은 해당하는 객체
+  //   return new Message(holder.getKey().intValue(), message.getText(), message.getCreatedDate());
+  // }
 }
